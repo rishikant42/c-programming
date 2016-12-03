a7ed4a3191342b102f5a83c74a8ebc5409111fc3
rishikant42 <rksbtp@gmail.com>
2016-12-03 18:42:24 +0530

    solved ex15

---
+++ Dennis-ritchie-C-language/ch1/ex15.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float ftoc(float fahr);
+
+int main()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+  lower = 0; 
+  upper = 300; 
+  step = 20; 
+  fahr = lower;
+
+  while (fahr <= upper) {
+    celsius = ftoc(fahr);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+
+  return 0;
+}
+
+float ftoc(float fahr)
+{
+  float celsius;
+  celsius = (5.0 / 9.0) * (fahr - 32.0);
+  return celsius;
+}
