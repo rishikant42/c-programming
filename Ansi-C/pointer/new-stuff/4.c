e092a1a6bd7ab4b6c6b35d2e3b0ffd56eb766ef8
rishikant42 <rksbtp@gmail.com>
2017-01-16 18:55:30 +0530

    basic poiter & struct program

---
+++ Ansi-C/pointer/new-stuff/4.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+typedef struct person
+{
+  int age;
+  float weight;
+};
+
+int main()
+{
+  struct person *personPtr;
+  struct person person1;
+
+  personPtr = &person1;
+
+  printf("Enter Integer: ");
+  scanf("%d", &personPtr->age);
+
+  printf("Enter number: ");
+  scanf("%f", &personPtr->weight);
+
+  printf("Displaying: ");
+  printf("%d  %f\n", personPtr->age, personPtr->weight);
+
+  return 0;
+}
