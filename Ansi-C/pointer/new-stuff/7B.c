d8e5085b237b301968b7e40f8952fc53c6e18023
rishikant42 <rksbtp@gmail.com>
2017-01-31 19:12:23 +0530

    string prointer & array realtion

---
+++ Ansi-C/pointer/new-stuff/7B.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(int argc, char** argv) {
+  char sptr[] = "Hello world";
+
+  int len = strlen(sptr);
+
+  printf("'%s' string have following characters: \n\n", sptr);
+  int i;
+  for(i=0;i<len;i++)
+    printf("The char-%d is %c\n", i+1, sptr[i]);
+
+  return 0;
+}
